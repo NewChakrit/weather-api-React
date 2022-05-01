84fc3de0f5c938ff656dc9bdec59b9b675833835
NewChakrit <imnew108@gmail.com>
2022-05-01 04:22:00 -0700

    'save'

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const name = "bangkok";
+  const apiKey = "f1526bf7197e521b723ec623b4a1aa47";
+
+  const [city, setCity] = useState({});
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}`;
+    fetch(url)
+      .then((res) => res.json())
+      // .then((data) => console.log(data));
+      .then((data) => setCity(data));
+  }, []);
+
+  console.log(city.weather.main);
+
+  const convertTemp = (k) => {
+    return (k - 273).toFixed();
+  };
   return (
     <div className="App">
       <section>
         <div className="location">
-          <h1 className="city">Bangkok</h1>
-          <p className="state">TH</p>
+          <h1 className="city">{city.name}</h1>
+          <p className="state">{city.sys.country}</p>
         </div>
 
         <div className="card">
           <div className="weather">
-            <h1>28</h1>
-            <small>max : 37, min : 20</small>
+            <h1>{convertTemp(city.main.temp)}&deg;C</h1>
+            <small>
+              max : {convertTemp(city.main.temp_max)}&deg;C , min :{" "}
+              {convertTemp(city.main.temp_min)}&deg;C
+            </small>
           </div>
           <div className="info">
-            <div className="status">Sunny</div>
-            <div className="humanity">100</div>
-            <div className="wind">4.0</div>
+            <div className="status">{city.weather[0].main}</div>
+            <div className="humadity">Humandity : {city.main.humidity}</div>
+            <div className="wind">Wind Speed : {city.wind.speed}</div>
           </div>
         </div>
       </section>
